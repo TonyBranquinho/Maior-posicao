a2bd38826187f3a879ba846e6b9782c716349234
Tony <antony.branquinho@gmail.com>
2023-12-06 16:30:14 -0300

    de segunda

---
+++ Maior posicao/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maior_posicao {
+    class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int n, i;
+            double maior, posicao;
+
+            Console.Write("Quantos numeros voce vai digitar? ");
+            n = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[n];
+
+            for (i = 0; i < n; i++) {
+                Console.Write("Digite um numero: ");
+                vet[i] = double.Parse(Console.ReadLine(), CI);
+            }
+
+            maior = vet[0];
+            posicao = 0;
+            for (i = 0; i < n; i++) {
+                if (vet[i] > maior) {
+                    maior = vet[i];
+                    posicao = i ;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("MAIOR VALOR = " + maior.ToString("F1", CI));
+            Console.WriteLine("POSICAO DO MAIOR VALOR = " + posicao);
+            Console.WriteLine();
+        }
+    }
+}
